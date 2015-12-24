718ba98ba77ae870dc5965c30df7e5337ef8510c
Hafiz Shafruddin <gmhafiz@gmail.com>
2015-12-24 23:21:08 +1100

    do not add advertisement already existed on database

--- gumtree.pl
+++ gumtree.pl
@@ -7,25 +7,31 @@
 # Search gumtree australia for specific keyword and send email 
 # notification if found.
 
-use strict;
+# use strict;
 use DBI;
+
+# Enter preferred email here
+my $email = 'gmhafiz@gmail.com';
 
 my $keyword = $ARGV[0];
 my $base_url =  "http://www.gumtree.com.au";
 my $url = "$base_url/s-$keyword/k0?fromSearchBox=true";
-my $email = 'gmhafiz@gmail.com';
 my $message = "";
+my $db_name = ".gumtree.db";
 
 my $dbh = DBI->connect(
-	"dbi:SQLite:dbname=gumtree.db",
+	"dbi:SQLite:dbname=.gumtree.db",
 	"",
 	"",
 	{ RaiseError =>1}
 ) or die $DBI::errstr;
 
-if (!-e "gumtree.db") {
-	$dbh->do("DROP TABLE IF EXISTS Items");
-	$dbh->do("CREATE TABLE Items(Id INT PRIMARY KEY, Name TEXT, Location TEXT, Url TEXT)");	
+if (-e "$db_name") {
+	# Db file exists
+} else {
+	# $dbh->do("DROP TABLE IF EXISTS Items");
+	$dbh->do("CREATE TABLE Items(Id INT PRIMARY KEY, Name TEXT, 
+									Location TEXT, Url TEXT)");	
 }
 
 sub main {
@@ -36,16 +42,14 @@ sub main {
 	my $source_code = `wget -O- $url 2>/dev/null`;
 	print MYFILE "$source_code";
 	close(MYFILE);
+	# TODO: concatenate multiple pages of search results
 
 	open F, "$source_file";
 	
 	my $found = 0;
-	my $num = 1;
+	my $num = 0;
 
 	while (my $line = <F>) {
-		# chomp $line;
-		# $line =~ s/\n//g;
-			
 		if ($line =~ m/$search_this/ig) {
 			$found = 1;
 			my @array_0  = split (/\"/, $line);  # capture url
@@ -55,28 +59,42 @@ sub main {
 			my $name 	 = $array_1[4];
 			my $id       = $array_1[5];
 			$id          =~ s/\">//;
-			# print "line: $line\n";
-			# print "$num\n"; 
-			# print "Name: $name\nLocation: $location\nId: $id";
-			# print "url: $base_url" . "$item_url\n";
 			$message .= "Name: $name\nLocation: $location\nId: $id" . 
 						"url: $base_url" . "$item_url\n\n";
 			$num += 1;
-			# send_email();
 			
-			# TODO: Query db and skip insert if $id is already present.
-			$dbh->do("INSERT INTO Items VALUES($id, '$name', '$location', '$item_url')");
+			my $sth = $dbh->prepare("SELECT Id FROM Items");
+			$sth->execute();
 			
+			my $existed = 0;
+			while(my $db_row = $sth->fetchrow_arrayref()) {
+				if ($id == @$db_row[0]) {  # Id is index 0
+					$existed = 1;
+				}
+			}
+			
+			if (!$existed) {
+				$dbh->do("INSERT INTO Items VALUES($id, '$name', 
+									'$location', '$item_url')");	
+			}
+			# TODO: delete removed advertisement from database
 		}
 	}
 	
 	if (!$found) {
 		print "Not found $keyword from $url, retry in 1 hour\n";
 	} else {
-		$num -= 1;
-		print "$num result(s) found\n\n";
-		print "$message\n";
+		if ($num == 0) {
+			print "No result found\n";
+		} elsif ($num == 1) {
+			print "$num result found\n\n";	
+		} else {
+			print "$num results found\n\n";
+		}
+		print "$message\n" . "sending email...\n";
 		send_email();
+		print "..done\n";
+		print "waiting for another hour for another query...\n";
 	}
 	sleep 3600;
 	close F;
